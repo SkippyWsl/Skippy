5db52c80b90056a2a63b7a1732f1b8d56c32aee8
SkippyWsl <112630434+SkippyWsl@users.noreply.github.com>
2023-11-28 16:23:48 +0100

    Add files via upload

---
+++ Skippy_UI/Advanced.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Advanced
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Advanced))
+        Me.tb_steps = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btn_cal = New System.Windows.Forms.Button()
+        Me.btn_sethome = New System.Windows.Forms.Button()
+        Me.btn_getx = New System.Windows.Forms.Button()
+        Me.tb_goto = New System.Windows.Forms.TextBox()
+        Me.btn_goto = New System.Windows.Forms.Button()
+        Me.delayms = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Ctimer = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tb_steps
+        '
+        Me.tb_steps.Location = New System.Drawing.Point(17, 16)
+        Me.tb_steps.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.tb_steps.Name = "tb_steps"
+        Me.tb_steps.Size = New System.Drawing.Size(132, 22)
+        Me.tb_steps.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(159, 20)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(133, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Step size in mm"
+        '
+        'btn_cal
+        '
+        Me.btn_cal.Location = New System.Drawing.Point(17, 49)
+        Me.btn_cal.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btn_cal.Name = "btn_cal"
+        Me.btn_cal.Size = New System.Drawing.Size(100, 28)
+        Me.btn_cal.TabIndex = 2
+        Me.btn_cal.Text = "Calibrate 0"
+        Me.btn_cal.UseVisualStyleBackColor = True
+        '
+        'btn_sethome
+        '
+        Me.btn_sethome.Location = New System.Drawing.Point(125, 49)
+        Me.btn_sethome.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btn_sethome.Name = "btn_sethome"
+        Me.btn_sethome.Size = New System.Drawing.Size(100, 28)
+        Me.btn_sethome.TabIndex = 3
+        Me.btn_sethome.Text = "Set Home"
+        Me.btn_sethome.UseVisualStyleBackColor = True
+        '
+        'btn_getx
+        '
+        Me.btn_getx.Location = New System.Drawing.Point(233, 49)
+        Me.btn_getx.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btn_getx.Name = "btn_getx"
+        Me.btn_getx.Size = New System.Drawing.Size(100, 28)
+        Me.btn_getx.TabIndex = 4
+        Me.btn_getx.Text = "Get X pos"
+        Me.btn_getx.UseVisualStyleBackColor = True
+        '
+        'tb_goto
+        '
+        Me.tb_goto.Location = New System.Drawing.Point(125, 87)
+        Me.tb_goto.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.tb_goto.Name = "tb_goto"
+        Me.tb_goto.Size = New System.Drawing.Size(132, 22)
+        Me.tb_goto.TabIndex = 5
+        '
+        'btn_goto
+        '
+        Me.btn_goto.Location = New System.Drawing.Point(16, 85)
+        Me.btn_goto.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btn_goto.Name = "btn_goto"
+        Me.btn_goto.Size = New System.Drawing.Size(100, 28)
+        Me.btn_goto.TabIndex = 6
+        Me.btn_goto.Text = "Go to"
+        Me.btn_goto.UseVisualStyleBackColor = True
+        '
+        'delayms
+        '
+        Me.delayms.Location = New System.Drawing.Point(143, 22)
+        Me.delayms.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.delayms.Name = "delayms"
+        Me.delayms.Size = New System.Drawing.Size(99, 22)
+        Me.delayms.TabIndex = 10
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Ctimer)
+        Me.GroupBox1.Controls.Add(Me.delayms)
+        Me.GroupBox1.Location = New System.Drawing.Point(16, 121)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.GroupBox1.Size = New System.Drawing.Size(276, 58)
+        Me.GroupBox1.TabIndex = 11
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Sync with PC"
+        '
+        'Ctimer
+        '
+        Me.Ctimer.AutoSize = True
+        Me.Ctimer.Checked = Global.Skippy_UI.My.MySettings.Default.timer
+        Me.Ctimer.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.Ctimer.DataBindings.Add(New System.Windows.Forms.Binding("Checked", Global.Skippy_UI.My.MySettings.Default, "timer", True, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged))
+        Me.Ctimer.Location = New System.Drawing.Point(13, 26)
+        Me.Ctimer.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Ctimer.Name = "Ctimer"
+        Me.Ctimer.Size = New System.Drawing.Size(112, 20)
+        Me.Ctimer.TabIndex = 11
+        Me.Ctimer.Text = "Timer ( in ms )"
+        Me.Ctimer.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(17, 187)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 28)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "Battery"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Advanced
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(357, 236)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btn_goto)
+        Me.Controls.Add(Me.tb_goto)
+        Me.Controls.Add(Me.btn_getx)
+        Me.Controls.Add(Me.btn_sethome)
+        Me.Controls.Add(Me.btn_cal)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.tb_steps)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Advanced"
+        Me.Text = "Advanced Settings"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents tb_steps As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btn_cal As Button
+    Friend WithEvents btn_sethome As Button
+    Friend WithEvents btn_getx As Button
+    Friend WithEvents tb_goto As TextBox
+    Friend WithEvents btn_goto As Button
+    Friend WithEvents delayms As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Ctimer As CheckBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Skippy_UI/Advanced.vb
@@ -0,0 +1,77 @@
+﻿Public Class Advanced
+
+    Private Sub btn_cal_Click(sender As Object, e As EventArgs) Handles btn_cal.Click
+        Skippy.Scom("Cal")
+    End Sub
+
+    Private Sub btn_sethome_Click(sender As Object, e As EventArgs) Handles btn_sethome.Click
+        Skippy.Scom("Set")
+    End Sub
+
+    Private Sub btn_getx_Click(sender As Object, e As EventArgs) Handles btn_getx.Click
+        Skippy.Smsg = String.Empty
+        Skippy.Scom("Getx")
+        While Skippy.Smsg = String.Empty
+            Application.DoEvents()
+        End While
+        MessageBox.Show("Current X position is " & Skippy.Smsg)
+    End Sub
+
+    Private Sub btn_goto_Click(sender As Object, e As EventArgs) Handles btn_goto.Click
+        If IsNumeric(tb_goto.Text) Then
+            Dim x
+            Dim target
+            Skippy.Smsg = String.Empty
+            Skippy.Scom("Getx")
+            While Skippy.Smsg = String.Empty
+                Application.DoEvents()
+            End While
+            x = Convert.ToInt32(Skippy.Smsg)
+            target = Convert.ToInt32(tb_goto.Text)
+            Skippy.Scom(target - x)
+        Else
+            MessageBox.Show("Go to Value is not numeric")
+        End If
+    End Sub
+
+    Private Sub Advanced_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tb_steps.Text = Skippy.stepwide
+        delayms.Text = Skippy.camwait
+        Label1.Select()
+    End Sub
+
+    Private Sub tb_steps_GotFocus(sender As Object, e As EventArgs) Handles tb_steps.GotFocus
+        tb_steps.ResetText()
+    End Sub
+
+    Private Sub tb_goto_GotFocus(sender As Object, e As EventArgs) Handles tb_goto.GotFocus
+        tb_goto.ResetText()
+    End Sub
+
+    Private Sub tb_steps_TextChanged(sender As Object, e As EventArgs) Handles tb_steps.TextChanged
+        If IsNumeric(tb_steps.Text) Then
+            Skippy.stepwide = tb_steps.Text
+        ElseIf tb_steps.Text = "" Then
+            Exit Sub
+        Else
+            MessageBox.Show("Step Value is not numeric")
+        End If
+    End Sub
+
+    Private Sub delayms_GotFocus(sender As Object, e As EventArgs) Handles delayms.GotFocus
+        delayms.ResetText()
+    End Sub
+    Private Sub delayms_TextChanged(sender As Object, e As EventArgs) Handles delayms.TextChanged
+        If IsNumeric(delayms.Text) Then
+            Skippy.camwait = delayms.Text
+        ElseIf delayms.Text = "" Then
+            Exit Sub
+        Else
+            MessageBox.Show("Step Value is not numeric")
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show(Cam.battleft)
+    End Sub
+End Class
